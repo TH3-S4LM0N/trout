e1173456f3f3f1d1147b0e8d0b652c00348b6ebb
TH3 S4LM0N <blakeblankenship@ccawarriors.com>
2022-12-18 20:40:49 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Gret from "./components/Greet.vue";
+</script>
+
+<template>
+    <div class="container">
+        <el-menu id="el-menu-tbar">
+            aaaaaaaaaaaa
+        </el-menu>
+
+        <h1>
+            Welcome to Tauri thing yeah
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+#el-menu-tbar {
+    border: none !important;
+    background-color: black;
+}
+</style>
